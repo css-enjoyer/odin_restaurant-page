8268e24af54bef64240e55f4fc24f159883158c4
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-01-24 10:24:30 +0800

    Add variable colors

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log("This is a test");
+
+
+function addComponent() {
+    const content = document.getElementById("content");
+    
+    // Create layout
+    const header = document.createElement("nav");
+    const hero = document.createElement("div");
+
+    content.appendChild(header);
+    content.appendChild(hero);
+    
+    // Create tabs
+    // const about = document.createElement("div");
+    // const menu = document.createElement("div");
+    // const contact = document.createElement("div");
+
+    
+
+};
+
+document.body.appendChild(addComponent());
